ce9592b05716138b1d4b971e142853e480bbb62f
Piki Alpian <pikialpian5@gmail.com>
2025-05-24 19:06:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -159,6 +159,8 @@ def download(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
